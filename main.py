bb5193367168250dcf8ea284cce94a09ef88dbdf
user <zhurakovskyi@nvk6.ukr.education>
2022-11-08 21:57:17 +0200

    Done 5 lb ex

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-x=int(input("x->"))
-y=int(input("y->"))
-
-if x<y == 0:
-print(f'{x} x<y')
-elif: x>y == 0
-print(f'{x} x>y')
+x=float(input("x->"))
+y=float(input("y->"))
+print('#>-----------<Menu>------------#')
+print(f'|+ : Show  sum for {x} and {y} |')
+print(f'|- : Show  dif for {x} and {y} |')
+print(f'|*: Show  mul for {x} and {y}  |')
+print(f'|/ : Show dif for {x} and {y}  |')
+print(f'|avg : Show avg for {x} and {y}|')
+print('#>-----------<Menu>------------#')
+action = input('action->')
+if action == '+':
+    print(f'{x} + {y} = {x + y}')
+elif action == '*':
+    print(f'{x} - {y} = {x * y}')
+elif action == '/':
+ if x == 0 or y == 0:
+    print(f"Error! {x} or {y} is zero!")
+else:
+    print(f'{x}/ {y} = {x/y}')
+    print(f'Avg = {(x+y)/2}')
