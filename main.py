a9e1400b4bca383e7e2fc23b5bab58b8b32edc54
user <zhurakovskyi@nvk6.ukr.education>
2022-11-08 22:04:43 +0200

    Done 1 HM ex

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 x=float(input("x->"))
 y=float(input("y->"))
-print('#>-----------<Menu>------------#')
-print(f'|+ : Show  sum for {x} and {y} |')
-print(f'|- : Show  dif for {x} and {y} |')
-print(f'|*: Show  mul for {x} and {y}  |')
-print(f'|/ : Show dif for {x} and {y}  |')
-print(f'|avg : Show avg for {x} and {y}|')
-print('#>-----------<Menu>------------#')
+z=float(input("z->"))
+print('#>-----------<Menu>--------------------#')
+print(f'|+ : Show  sum for {x} and {y} and {z} |')
+print(f'|*: Show  mul for {x} and {y}  and {z} |')
+print('#>-----------<Menu>--------------------#')
 action = input('action->')
 if action == '+':
-    print(f'{x} + {y} = {x + y}')
+    print(f'{x} + {y} + {z}= {x + y + z}')
 elif action == '*':
-    print(f'{x} - {y} = {x * y}')
-elif action == '/':
- if x == 0 or y == 0:
-    print(f"Error! {x} or {y} is zero!")
-else:
-    print(f'{x}/ {y} = {x/y}')
-    print(f'Avg = {(x+y)/2}')
+    print(f'{x} * {y} * {z}= {x * y * z}')
