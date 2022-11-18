6c6240d4e8f3bb946449aae64dc61ad104c2a185
user <zhurakovskyi@nvk6.ukr.education>
2022-11-18 09:53:26 +0200

    Done 2 HM ex

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ x=float(input("x->"))
 y=float(input("y->"))
 z=float(input("z->"))
 print('#>-----------<Menu>--------------------#')
-print(f'|+: Show   for {x} and {y} and {z} |')
-print(f'|*: Show   for {x} and {y}  and {z} |')
-print(f'|: Show   for {x} and {y}  and {z} |')
+print(f'|+: Show   + for {x} and {y} and {z} |')
+print(f'|*: Show  * for {x} and {y}  and {z} |')
+print(f'|/: Show avg for {x} and {y}  and {z} |')
 print('#>-----------<Menu>--------------------#')
 action = input('action->')
 if action == '+':
     print(f'{x} + {y} + {z}= (x + y + z')
 elif action == '*':
     print(f'{x} * {y} * {z}= {x * y * z}')
+elif action == '/':
+    print(f'{x}+{y}+{z}/{2}= {x+y+z/2}')
