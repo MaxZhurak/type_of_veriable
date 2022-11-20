36b2d6b7e4ff263d47051a171a0c735849de93db
user <zhurakovskyi@nvk6.ukr.education>
2022-11-20 19:48:20 +0200

    Done 2 HM ex

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-print('#>---------<Menu>--------#')
-print(f'|Понеділок: Show for {1} |')
-print(f'|Вівторок: Show for {2}  |')
-print(f'|Середа: Show for {3}    |')
-print(f'|Четверг: Show for {4}   |')
-print(f'|Пятниця: Show for {5}   |')
-print(f'|Субота: Show for {6}    |')
-print(f'|Неділя: Show for {7}    |')
-print('#>---------<Menu>--------#')
+print('#>---------<Menu>---------#')
+print(f'|Січень: Show for {1}    |')
+print(f'|Лютий: Show for {2}     |')
+print(f'|Березень: Show for {3}  |')
+print(f'Квітень: Show for {4}    |')
+print(f'|Травень: Show for {5}   |')
+print(f'|Червень: Show for {6}   |')
+print(f'|Липень: Show for {7}    |')
+print(f'|Серпень: Show for {8}   |')
+print(f'|Вересень: Show for {9}  |')
+print(f'|Жовтень: Show for {10}  |')
+print(f'|Листопад: Show for {11} |')
+print(f'|Грудень: Show for {12}  |')
+print('#>---------<Menu>---------#')
 action = input('action->')
 if action == '1':
-    print('Понеділок')
+    print('Січень')
 elif action == '2':
-    print('Вівторок')
+    print('Лютий')
 elif action == '3':
-    print('Середа ')
+    print('Березень')
 elif action == '4':
-    print('Четверг')
+    print('Квітень')
 elif action == '5':
-    print('Пятниця')
+    print('Травень')
 elif action == '6':
-    print('Субота')
+    print('Червень')
 elif action == '7':
-    print('Неділя')
+    print('Липень')
+elif action == '8':
+    print('Серпень')
+elif action == '9':
+    print('Вересень')
+elif action == '10':
+    print('Жовтень')
+elif action == '11':
+    print('Листопад')
+elif action == '12':
+    print('Грудень')
