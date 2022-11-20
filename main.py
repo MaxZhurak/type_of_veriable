e32bf1df630d2958aefcd16fe5dc6b30b0122f3b
user <zhurakovskyi@nvk6.ukr.education>
2022-11-20 19:40:54 +0200

    Done 1 HM ex

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-x=float(input("x->"))
-print('#>----------------<Menu>------------------#')
-print(f'|mile: Show mile for {x} and {0.6}       |')
-print(f'|inches: Show inches for {x} and {35.37} |')
-print(f'|yards: Show yards for {x} and {0.9}     |')
-print('#>----------------<Menu>------------------#')
+print('#>---------<Menu>--------#')
+print(f'|Понеділок: Show for {1} |')
+print(f'|Вівторок: Show for {2}  |')
+print(f'|Середа: Show for {3}    |')
+print(f'|Четверг: Show for {4}   |')
+print(f'|Пятниця: Show for {5}   |')
+print(f'|Субота: Show for {6}    |')
+print(f'|Неділя: Show for {7}    |')
+print('#>---------<Menu>--------#')
 action = input('action->')
-if action == 'mile':
-    print(f'{x} * {0.6}= {x * 0.6}')
-elif action == 'inches':
-    print(f'{x} * {35.37}= {x * 35.37}')
-elif action == 'yards':
-    print(f'{x} * {0.9}= {x*0.9}')
+if action == '1':
+    print('Понеділок')
+elif action == '2':
+    print('Вівторок')
+elif action == '3':
+    print('Середа ')
+elif action == '4':
+    print('Четверг')
+elif action == '5':
+    print('Пятниця')
+elif action == '6':
+    print('Субота')
+elif action == '7':
+    print('Неділя')
