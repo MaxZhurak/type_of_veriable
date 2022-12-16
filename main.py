d804c6826c228d9e0541ee636491abd8535e8f11
user <zhurakovskyi@nvk6.ukr.education>
2022-12-16 22:12:23 +0200

    Done 3 lb ex

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ try:
  begin = int(input('begin->'))
  end = int(input('end->'))
  for item in range(begin, end+1):
-    if item % 2 != 0:
+    if item % 2 == 0:
      print(item, end = " ")
+
+except ValueError as vl_ex:
+    print(f'ValueError: {vl_ex}')
+except Exception as ex:
+    print(f'Eror: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
+
+
+
+
