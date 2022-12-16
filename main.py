d5166772f8e6771546cfcb3dd4a23291f69c259a
user <zhurakovskyi@nvk6.ukr.education>
2022-12-16 22:09:23 +0200

    Done 1 lb ex

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
-print('#>---------<Menu>---------#')
-print(f'|Січень: Show for {1}    |')
-print(f'|Лютий: Show for {2}     |')
-print(f'|Березень: Show for {3}  |')
-print(f'Квітень: Show for {4}    |')
-print(f'|Травень: Show for {5}   |')
-print(f'|Червень: Show for {6}   |')
-print(f'|Липень: Show for {7}    |')
-print(f'|Серпень: Show for {8}   |')
-print(f'|Вересень: Show for {9}  |')
-print(f'|Жовтень: Show for {10}  |')
-print(f'|Листопад: Show for {11} |')
-print(f'|Грудень: Show for {12}  |')
-print('#>---------<Menu>---------#')
-action = input('action->')
-if action == '1':
-    print('Січень')
-elif action == '2':
-    print('Лютий')
-elif action == '3':
-    print('Березень')
-elif action == '4':
-    print('Квітень')
-elif action == '5':
-    print('Травень')
-elif action == '6':
-    print('Червень')
-elif action == '7':
-    print('Липень')
-elif action == '8':
-    print('Серпень')
-elif action == '9':
-    print('Вересень')
-elif action == '10':
-    print('Жовтень')
-elif action == '11':
-    print('Листопад')
-elif action == '12':
-    print('Грудень')
+try:
+begin = int(input('begin->'))
+end = int(input('end->'))
+for item in range(begin+1, end):
+    print(item, end='\t')
