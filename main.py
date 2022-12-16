504d459b59663c93756c3980915e677ba4dc39f8
user <zhurakovskyi@nvk6.ukr.education>
2022-12-16 22:14:53 +0200

    Done 5 lb ex

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-try:
- begin = int(input('begin->'))
- end = int(input('end->'))
- for item in reversed(range(begin, end+1)):
-     print(item, end = " ")
-
-except ValueError as vl_ex:#
- print(f'ValueError: {vl_ex}')
-except Exception as ex:
-    print(f'Eror: {ex}')
-   print(f'Name: {ex.__class__.__name__}')
+begin = int(input('begin->'))
+end = int(input('end->'))
+counter = int(0)
+for item in range(begin+1, end):
+    print(item, end=" ")
+    if item % 3 == 0:
+        print('fizz', end=' ')
+    if item % 5 == 0:
+        print('buzz', end=' ')
+   print()
