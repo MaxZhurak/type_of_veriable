dd07697e70bca419ba064572e13086c6bb7e49b3
user <zhurakovskyi@nvk6.ukr.education>
2022-12-16 22:10:17 +0200

    Done 2 lb ex

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 try:
-begin = int(input('begin->'))
-end = int(input('end->'))
-for item in range(begin+1, end):
-    print(item, end='\t')
+ begin = int(input('begin->'))
+ end = int(input('end->'))
+ for item in range(begin, end+1):
+    if item % 2 != 0:
+     print(item, end = " ")
