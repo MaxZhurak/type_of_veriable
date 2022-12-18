4c095fa5a1425054441c740bb9a395ec3649e446
user <zhurakovskyi@nvk6.ukr.education>
2022-12-18 18:21:43 +0200

    Done 2 lb ex

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 try:
     start = int(input('start->'))
     end = int(input('end->'))
-    sum = float(0)
     counter = 0
     for i in range(start, end+1):
-        sum += i
-        counter += 1
-        print(counter)
-    print(f'sum + {sum}, Avg = {sum} /  {counter} = {sum/counter}')
+        if i % 5 ==0:
+            print(i,end=' ')
+            counter+=1
+    print()
+    print(counter)
 except Exception as es:
-    print(f'Eror [{ex.__class__.__name__}]: {ex}')
+    print(f'Eror: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
