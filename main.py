9f0347aac6dad6b8aae276b2d7dc21c85a1c8691
user <zhurakovskyi@nvk6.ukr.education>
2022-12-18 18:20:35 +0200

    Done 1 lb ex

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-begin = int(input('begin->'))
-end = int(input('end->'))
-counter = int(0)
-for item in range(begin+1, end):
-    print(item, end=" ")
-    if item % 3 == 0:
-        print('fizz', end=' ')
-    if item % 5 == 0:
-        print('buzz', end=' ')
-   print()
+try:
+    start = int(input('start->'))
+    end = int(input('end->'))
+    sum = float(0)
+    counter = 0
+    for i in range(start, end+1):
+        sum += i
+        counter += 1
+        print(counter)
+    print(f'sum + {sum}, Avg = {sum} /  {counter} = {sum/counter}')
+except Exception as es:
+    print(f'Eror [{ex.__class__.__name__}]: {ex}')
