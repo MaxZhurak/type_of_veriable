6fb44026ccc5cc3d455a20798987db5baf6a5883
user <zhurakovskyi@nvk6.ukr.education>
2022-12-18 18:22:23 +0200

    Done 3 lb ex

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 try:
-    start = int(input('start->'))
-    end = int(input('end->'))
-    counter = 0
-    for i in range(start, end+1):
-        if i % 5 ==0:
-            print(i,end=' ')
-            counter+=1
+    lenght = int(input('length->'))
+    sign = input('sign->')
+    for i in range(0, lenght):
+        print(sign, end='')
     print()
-    print(counter)
 except Exception as es:
     print(f'Eror: {ex}')
     print(f'Name: {ex.__class__.__name__}')
