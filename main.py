3a589eabafa580d0e9436660f4800869cca4033c
user <zhurakovskyi@nvk6.ukr.education>
2022-12-18 18:25:27 +0200

    Done 4 HM ex

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 try:
-    lenght = int(input('length->'))
-    sign = input('sign->')
-    for i in range(0, lenght):
-        print(sign, end='')
+    number = 0
+    sum = 0
+    min = 0
+    max = 0
+    triger = True
+    while True:
+        number = int(input('number->'))
+        if number == 7:
+            break
+        else:
+            if triger:
+                min_ = max_ = number
+                triger = False
+            else:
+                sum += number
+                if max < number:
+                    max = number
+                if min > number:
+                    min = number
+    print(f'Sum = {sum}')
+    print(f'Min = {min}')
+    print(f'Max = {max}')
 except Exception as es:
-    print(f'Eror: {ex}')
-    print(f'Name: {ex.__class__.__name__}')
+    print(f'Error [{ex.__class__.__name__}]: {ex}')
