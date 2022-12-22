6d1a11368fa3860f30644cc853ee1bd3b0af974b
user <zhurakovskyi@nvk6.ukr.education>
2022-12-22 18:29:44 +0200

    Done

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-try:
-    number = 0
-    sum = 0
-    min = 0
-    max = 0
-    triger = True
-    while True:
-        number = int(input('number->'))
-        if number == 7:
-            break
-        else:
-            if triger:
-                min_ = max_ = number
-                triger = False
-            else:
-                sum += number
-                if max < number:
-                    max = number
-                if min > number:
-                    min = number
-    print(f'Sum = {sum}')
-    print(f'Min = {min}')
-    print(f'Max = {max}')
-except Exception as es:
-    print(f'Error [{ex.__class__.__name__}]: {ex}')
+time= float(input("time->"))
+name= int(input("name->"))
+if time == '8:00':
+    print("Доброго ранку {name} зараз {time}")
+else: time == '12:00'
+print("Доброго дня {name} зараз {time}")
+elif:  time == '18:00'
+print("Доброго вечора {name} зараз {time} ")
+elif: time == '00:00'
+print("Доброї ночі {name} зараз {time}")
+elif: time == '1:00'
