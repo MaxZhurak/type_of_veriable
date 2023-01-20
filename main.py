3ff80587f96efed8eb7d1d54440ed897b61458f8
user <zhurakovskyi@nvk6.ukr.education>
2023-01-20 12:05:36 +0200

    Done ex3

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 try:
-    for i in range(1, 6):
-        for j in range(1, 6):
-            print('+', end=" ")
+    col = int(input('col->'))
+    row = int(input('row->'))
+    for i in range(0, col):
+        for j in range(0, row):
+            if i == 0 or i == col-1:
+                print('+', end=" ")
+        else:
+            if j == 0 or j == row-1:
+                print('*', end=" ")
+            else:
+                print(' ', end=" ")
         print()
 except Exception as ex:
     print(f'Error [{ex.__class__.__name}]:{ex}')
