77438060171ae7a0d9acd8b39dfd08c3b5f34dc1
user <zhurakovskyi@nvk6.ukr.education>
2023-01-24 13:45:29 +0200

    Done ex2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 try:
-    start= int(input('start->'))
-    end = int(input('end->'))
-    for number in range(start, end+1):
-        counter = 0
-        for i in range(1, number+1):
-            if number % i == 0:
-                counter += 1
-        if counter == 2:
-                print(number, end=" ")
+    number = int(input('number->'))
+    for i in range(1, 10+1):
+        for j in range(1, 10+1):
+                print(f'{i} * {j} = {i*j}', end="\t\t")
         print()
+        for j in range(1, 161):
+                print(f'-', end="")
+        print()
+    print()
 except Exception as ex:
     print(f'Error [{ex.__class__.__name}]:{ex}')
