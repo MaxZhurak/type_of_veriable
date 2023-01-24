11a469331a31f6fea7c774394d73ce9ee9822585
user <zhurakovskyi@nvk6.ukr.education>
2023-01-24 13:26:30 +0200

    Done ex1

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 try:
-    col = int(input('col->'))
-    row = int(input('row->'))
-    for i in range(0, col):
-        for j in range(0, row):
-            if i == 0 or i == col-1:
-                print('+', end=" ")
-        else:
-            if j == 0 or j == row-1:
-                print('*', end=" ")
-            else:
-                print(' ', end=" ")
+    start= int(input('start->'))
+    end = int(input('end->'))
+    for number in range(start, end+1):
+        counter = 0
+        for i in range(1, number+1):
+            if number % i == 0:
+                counter += 1
+        if counter == 2:
+                print(number, end=" ")
         print()
 except Exception as ex:
     print(f'Error [{ex.__class__.__name}]:{ex}')
