7592c73fc9fe1b61ad56aaf425835d7966c3c5a9
user <zhurakovskyi@nvk6.ukr.education>
2023-02-07 13:17:09 +0200

    Done ex3

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-print("Введіть початок та кінець діапазону")
-start = int(input("Початок:"))
-end = int(input("Кінець:"))
-print("Усі числа діапазону:")
+start = int(input("Enter start of range:"))
+end = int(input("Enter end of range:"))
 for i in range(start,end + 1):
-        print(i,end=" ")
-    print("Усі числа діапазону за спаданням:")
-    for i in range(end,start - 1, -1):
-        print(i,end=" ")
-    print()
-    print("Усі числа, кратні 7:")
-    for i in range(start,end + 1):
-        if i % 7 == 0:
-            print(i,end=" ")
-    print("Кількість чисел, кратних 5:")
-    count = 0
-    for i in range(start,end + 1):
-        if i % 5 == 0:
-            count += 1
-    print(count)
-if __name__ == "__main__":
-    main()
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizz Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+else:
+            print(i)
