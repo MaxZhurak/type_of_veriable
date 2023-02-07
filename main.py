9f132e05acb552c771ff8a6cdbf4d37cb1c27971
user <zhurakovskyi@nvk6.ukr.education>
2023-02-07 13:11:51 +0200

    Done ex2

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-a = int(input('Введіть продажі 1 менеджера: '))
-b = int(input('Введіть продажі 2 менеджера: '))
-c = int(input('Введіть продажі 3 менеджера: '))
-premium = 200
-if a>1000:
-    zp1: float = oklad+a*0.08
-else:
-    a <500
-zp1 = oklad+a*0.03
-else:
-    zp1 = oklad+a*0.05
-if b>1000:
-    zp2 = oklad+b*0.08
-else:
-    b <500:
-zp2 = oklad+b*0.03
-else:
-    zp2 = oklad+b*0.05
-if c>1000:
-    zp3 = oklad+c*0.08
-else:
-    c <500:
-zp3 = oklad+c*0.03
-else:
-    zp3 = oklad+c*0.05
-if zp1 > zp2 and zp1 > zp3:
-print('Кращій  продажам - 1 менеджер!')
-zp1 += 200
-if zp2 > zp1 and zp2 > zp3:
-print('Кращій по продажам - 2 менеджер!')
-zp2 += 200
-if zp3 > zp1 and zp3 > zp2:
-print('Кращій по продажам - 3 менеджер!')
-zp3 +=200
-print('Заробітьня плата 1 менеджера ',zp1)
-print('Заробітьня плата 2 менеджера ',zp2)tors[operator2]
+print("Введіть початок та кінець діапазону")
+start = int(input("Початок:"))
+end = int(input("Кінець:"))
+print("Усі числа діапазону:")
+for i in range(start,end + 1):
+        print(i,end=" ")
+    print("Усі числа діапазону за спаданням:")
+    for i in range(end,start - 1, -1):
+        print(i,end=" ")
+    print()
+    print("Усі числа, кратні 7:")
+    for i in range(start,end + 1):
+        if i % 7 == 0:
+            print(i,end=" ")
+    print("Кількість чисел, кратних 5:")
+    count = 0
+    for i in range(start,end + 1):
+        if i % 5 == 0:
+            count += 1
+    print(count)
+if __name__ == "__main__":
+    main()
