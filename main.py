bffe4e7e2573575a9051e3200054f6bef4830093
user <zhurakovskyi@nvk6.ukr.education>
2023-02-07 13:02:09 +0200

    Done ex4

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-operators = {
-   'Vodafone': 0.66,
-
-   'Kyivstar': 0.45,
-
-   'Lifecell': 0.76,
-}
-int(input("Введіть вартість розмови: ")), input("Виберіть оператор: "), input("Виберіть другий оператор: ")
-if operators1 == operators2:
-   print("Вартість розмови: ", cost)
+a = int(input('Введіть продажі 1 менеджера: '))
+b = int(input('Введіть продажі 2 менеджера: '))
+c = int(input('Введіть продажі 3 менеджера: '))
+oklad = 200
+if a>1000:
+    zp1: float = oklad+a*0.08
 else:
-   print("Вартість розмови: ", cost * operators[operator1] + cost * operators[operator2])
+    a <500
+zp1 = oklad+a*0.03
+else:
+    zp1 = oklad+a*0.05
+if b>1000:
+    zp2 = oklad+b*0.08
+else:
+    b <500:
+zp2 = oklad+b*0.03
+else:
+    zp2 = oklad+b*0.05
+if c>1000:
+    zp3 = oklad+c*0.08
+else:
+    c <500:
+zp3 = oklad+c*0.03
+else:
+    zp3 = oklad+c*0.05
+if zp1 > zp2 and zp1 > zp3:
+print('Кращій  продажам - 1 менеджер!')
+zp1 += 200
+if zp2 > zp1 and zp2 > zp3:
+print('Кращій по продажам - 2 менеджер!')
+zp2 += 200
+if zp3 > zp1 and zp3 > zp2:
+print('Кращій по продажам - 3 менеджер!')
+zp3 +=200
+print('Заробітьня плата 1 менеджера ',zp1)
+print('Заробітьня плата 2 менеджера ',zp2)tors[operator2]
