1b25917c3b02b600aa56275b47b156b4a4ffaac1
user <zhurakovskyi@nvk6.ukr.education>
2023-02-14 13:36:39 +0200

    done ex 3

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-input_string = input("Введіть рядок: ")
-if input_string == input_string[::-1]:
- print("Введений рядок є паліндромом")
-else:
- print("Введений рядок не є паліндромом")
+text = input("Введіть текст: ")
+num_sentences = text.count('.') + text.count('!') + text.count('?')
+print(f"Кількість пропозицій у тексті: {num_sentences}")
